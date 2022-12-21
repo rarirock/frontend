c3032cdf05aab06cd06fe463dab19815d9b6f87c
Judy's Dad <mine0202@naver.com>
2022-12-21 10:31:25 +0900

    css 수정, 컴포넌트 만듬

--- src/components/reservation/PaymentView.vue
+++ src/components/reservation/PaymentView.vue
@@ -275,7 +275,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped >
 
 .wrap_reserve {
     overflow: hidden;
--- src/components/reservation/SeatReservation.vue
+++ src/components/reservation/SeatReservation.vue
@@ -2197,7 +2197,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped >
 .wrap_reserve {
     overflow: hidden;
     position: relative;
